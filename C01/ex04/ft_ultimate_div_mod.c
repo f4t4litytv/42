d984cc3da6c0bd5d3bbeaab9eebdb1f2330a4849
jpinto-r <jpinto-r@student.42lisboa.com>
2025-03-10 19:49:16 +0000

    updates

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -6,7 +6,7 @@
 /*   By: jpinto-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 18:33:02 by jpinto-r          #+#    #+#             */
-/*   Updated: 2025/03/10 18:48:00 by jpinto-r         ###   ########.fr       */
+/*   Updated: 2025/03/10 19:48:18 by jpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,25 @@
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	int	c;
-	int	d;
+	int div;
+	int mod;
 
-	c = 10;
-	d = 40;
-	*a = c / d;
-	*b = c % d;
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
 }
 /*
 int main(void)
 {
-	int a, b;
+	int x, y;
+	x = 10;
+	y = 3;
 
-	ft_ultimate_div_mod(&a, &b);
-	printf("divisao = %d\nresto = %d \n", a, b);
-}
-*/
+
+	ft_ultimate_div_mod(&x, &y);
+	printf("divisao = %d\nresto = %d \n", x, y);
+
+	return 0;
+}*/
+
