68ace9968f56cdb63edc06dfdc3bdd4b03ddd071
jpinto-r <jpinto-r@student.42lisboa.com>
2025-03-10 18:14:30 +0000

    ex01 updated

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpinto-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 17:46:01 by jpinto-r          #+#    #+#             */
+/*   Updated: 2025/03/10 18:00:29 by jpinto-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+int	main(void)
+{
+	int	n = 1; // criamos uma var para colocar o endereco
+	ft_ft(&n); // chamamos a funcao com endereco que queremos obter
+	printf("O pointer nbr e : %d \n", n);
+	// agora printamos o apontador da var n que passara *nbr 42
+	return (0);
+}
--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: f4t4lity <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jpinto-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/08 00:15:57 by f4t4lity          #+#    #+#             */
-/*   Updated: 2025/03/09 16:06:21 by jpinto-r         ###   ########.fr       */
+/*   Created: 2025/03/10 18:01:59 by jpinto-r          #+#    #+#             */
+/*   Updated: 2025/03/10 18:13:52 by jpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,37 @@ void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42 ;
 }
-/*
+
 int main()
 {
 	int num = 0;
+	// declaramos a var num com valor 0
 	int *ptr1 = &num;
+	// declaramos a o apontador ptr1 para a var anterior
 	int **ptr2 = &ptr1;
+	// declaramos a o apontador ptr2 para a var anterior
 	int ***ptr3 = &ptr2;
+	// declaramos a o apontador ptr3 para a var anterior	
 	int ****ptr4 = &ptr3;
-	int *****ptr5 = &ptr4;
+	// declaramos a o apontador ptr4 para a var anterior
+	int *****ptr5 = &ptr4;	
+	// declaramos a o apontador ptr5 para a var anterior
 	int ******ptr6 = &ptr5;
+	// declaramos a o apontador ptr6 para a var anterior
 	int *******ptr7 = &ptr6;
+	// declaramos a o apontador ptr7 para a var anterior
 	int ********ptr8 = &ptr7;
-	int *********ptr9 = &ptr8;
+	// declaramos a o apontador ptr8 para a var anterior
+	int *********ptr9 = &ptr8;	
+	// declaramos a o apontador ptr9 para a var anterior
 
     ft_ultimate_ft(ptr9);
-
+	/*
+	   agora chamamos a funcao com o parametro do ultimo 
+	   pointer para conseguirmos 
+	   printar 42 com o pointer *********nbr
+	*/
     printf("O valor de num é: %d\n", num);
 
     return 0;
-}*/
+}
