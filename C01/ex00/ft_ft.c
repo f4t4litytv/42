9117af2b7c13871f71ba7e5406371ec4aeaaba47
jpinto-r <jpinto-r@student.42lisboa.com>
2025-03-10 17:39:27 +0000

    update header

--- C01/ex00/ft_ft.c
+++ C01/ex00/ft_ft.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: f4t4lity <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jpinto-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/07 15:55:48 by f4t4lity          #+#    #+#             */
-/*   Updated: 2025/03/09 16:03:32 by jpinto-r         ###   ########.fr       */
+/*   Created: 2025/03/10 17:38:37 by jpinto-r          #+#    #+#             */
+/*   Updated: 2025/03/10 17:38:42 by jpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
@@ -21,14 +22,7 @@ int	main(void)
 {
 	int n = 1;
 	int *nbr = &n;
-
-	printf("Before ft_ft: \n");
-	printf("Variable 'nbr' points to adress %p that has the value %d stored\n", &n, n);
-	printf("Pointer 'nbr' points to the same adress %p that has the value %d stored\n", nbr, *nbr);
-	ft_ft(nbr);
-	printf("\n");
-	printf("After ft_ft: \n");
-	printf("Pointer 'nbr' remains pointing to adress %p, but now that has the value %d stored\n", nbr, *nbr);
-	printf("Now 'n' has the value %d stored\n", n);
+	write(1, nbr, 2);
+	return 0;
 }
 
