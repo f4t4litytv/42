7bb0faee5bc97aa0aa75f2b7156297e8b5676407
jpinto-r <jpinto-r@student.42lisboa.com>
2025-03-11 18:35:18 +0000

    C02

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -1,32 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpinto-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/11 17:25:56 by jpinto-r          #+#    #+#             */
-/*   Updated: 2025/03/11 17:43:59 by jpinto-r         ###   ########.fr       */
+/*   Created: 2025/03/11 18:29:36 by jpinto-r          #+#    #+#             */
+/*   Updated: 2025/03/11 18:30:10 by jpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_lowercase(char *str)
 {
-	if (*str == '\0')
+	if (str[0] == '\0')
 	{
 		return 1;
 	}
 	
-	while(*str)
+	int i = 0;
+	while(str[i] != '\0')
 	{
-		if (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z')))
+		if (str[i] < 'a' || str[i] > 'z')
 		{
 			return 0;
 		}
-		str++;
+		i++;
 	}
 	return 1;
 }
+
