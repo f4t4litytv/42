6558c63f8cc687bf9dcbda4e521fae0256bc0dc2
f4t4litytv <f4t4litytv@gmail.com>
2025-03-12 01:59:59 +0000

    ate ao ex04

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -6,7 +6,7 @@
 /*   By: jpinto-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 18:29:36 by jpinto-r          #+#    #+#             */
-/*   Updated: 2025/03/12 01:43:10 by f4t4lity         ###   ########.fr       */
+/*   Updated: 2025/03/12 01:47:45 by jpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,16 @@ int     main()
         char    str4[] = ""; 
         char    str5[] = "!#$%%&/(()";  ; 
  
-        printf("str1 (\"%s\") é minúscula : %d\n", str1, ft_str_is_lowercase(str1)); 
-        printf("str2 (\"%s\") é minúscula : %d\n", str2, ft_str_is_lowercase(str2)); 
-        printf("str3 (\"%s\") é minúscula : %d\n", str3, ft_str_is_lowercase(str3)); 
-        printf("str4 (\"%s\") é minúscula : %d\n", str4, ft_str_is_lowercase(str4)); 
-        printf("str5 (\"%s\") é minúscula : %d\n", str5, ft_str_is_lowercase(str5)); 
+        printf("str1 (\"%s\") é minúscula : 
+%d\n", str1, ft_str_is_lowercase(str1)); 
+        printf("str2 (\"%s\") é minúscula : 
+%d\n", str2, ft_str_is_lowercase(str2)); 
+        printf("str3 (\"%s\") é minúscula :
+ %d\n", str3, ft_str_is_lowercase(str3)); 
+        printf("str4 (\"%s\") é minúscula :
+ %d\n", str4, ft_str_is_lowercase(str4)); 
+        printf("str5 (\"%s\") é minúscula :
+ %d\n", str5, ft_str_is_lowercase(str5)); 
         return 0; 
 } 
 */
