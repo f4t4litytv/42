ac4ee4688ac93127c42d177f8d6311450ee1ceca
f4t4lity <f4t4lity@f4t4lity-G5-KD>
2025-03-13 15:15:20 +0000

    ex09

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: f4t4lity <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 20:44:39 by f4t4lity          #+#    #+#             */
-/*   Updated: 2025/03/13 00:35:31 by f4t4lity         ###   ########.fr       */
+/*   Updated: 2025/03/13 14:27:04 by f4t4lity         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,38 @@
 
 char	*ft_strcapitalize(char *str)
 {
-	int	i = 0, int	primeira_palavra = 1;
+	int	i = 0, primeira_palavra = 1;
 
 	while (str[i] != '\0')
 	{
-		if (isalnum(str[i])) {
+		if (isalnum(str[i]))
+		{
 			if (primeira_palavra)
 			{
-				str[i] = toupper(str[i]), primeira_palavra = 0;
-			
-			} else {
-				str[i] = tolower(str[iI]);
-			} else {
-				primeira_palavra = 1;
-				}
-				i++;
+				str[i] = toupper(str[i]);
+				primeira_palavra = 0;
+			}
+			else
+			{
+				str[i] = tolower(str[i]);
+			}
+			i++;
 		}
-		return (str);
+		else
+		{
+			primeira_palavra = 1;
+			i++;
+		}
 	}
+	return (str);
 }
 /*
-int main()
+int	main()
 {
 	char	str1[] = "ola, tudo bem?"; 
 	char	str2[] = "PRIMEIRA PALAVRA"; 
 	char	str3[] = "outra string";
-       	char	str4[] = "123palavras"; char
+       	char	str4[] = "123palavras";
 	char	str5[] = "";
 	
 	char	*copia1 = strdup(str1);
@@ -57,7 +63,7 @@ int main()
     '%s'\n", str2, ft_strcapitalize(copia2));
     printf("Original: '%s', Capitalizada:
     '%s'\n", str3, ft_strcapitalize(copia3));
-    printf("Original: '%s', Capitalizada:
+    printf("Original: '%s', Capitalizada: 
     '%s'\n", str4, ft_strcapitalize(copia4));
     printf("Original: '%s', Capitalizada:
     '%s'\n", str5, ft_strcapitalize(copia5));
