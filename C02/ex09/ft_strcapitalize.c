e49f7418febf8842b36d6d15ad93c899f34f7209
f4t4litytv <f4t4litytv@gmail.com>
2025-03-13 01:23:50 +0000

    ateao09

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: f4t4lity <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 20:44:39 by f4t4lity          #+#    #+#             */
-/*   Updated: 2025/03/13 00:35:31 by f4t4lity         ###   ########.fr       */
+/*   Updated: 2025/03/13 01:22:45 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-char	*ft_strcapitalize(char *str)
-{
-	int	i = 0, int	primeira_palavra = 1;
+char *ft_strcapitalize(char *str) {
+    int i = 0;
+    int primeira_palavra = 1;
 
-	while (str[i] != '\0')
-	{
-		if (isalnum(str[i])) {
-			if (primeira_palavra)
-			{
-				str[i] = toupper(str[i]), primeira_palavra = 0;
-			
-			} else {
-				str[i] = tolower(str[iI]);
-			} else {
-				primeira_palavra = 1;
-				}
-				i++;
-		}
-		return (str);
-	}
+    while (str[i] != '\0') {
+        if (isalnum(str[i])) {
+            if (primeira_palavra) {
+                str[i] = toupper(str[i]);
+                primeira_palavra = 0;
+            } else {
+                str[i] = tolower(str[i]);
+            }
+        } else {
+            primeira_palavra = 1;
+        }
+        i++;
+    }
+    return (str);
 }
 /*
 int main()
@@ -42,7 +40,7 @@ int main()
 	char	str1[] = "ola, tudo bem?"; 
 	char	str2[] = "PRIMEIRA PALAVRA"; 
 	char	str3[] = "outra string";
-       	char	str4[] = "123palavras"; char
+       	char	str4[] = "123palavras";
 	char	str5[] = "";
 	
 	char	*copia1 = strdup(str1);
@@ -51,16 +49,11 @@ int main()
 	char	*copia4 = strdup(str4);
 	char	*copia5 = strdup(str5);
 
-    printf("Original: '%s', Capitalizada:
-    '%s'\n", str1, ft_strcapitalize(copia1));
-    printf("Original: '%s', Capitalizada:
-    '%s'\n", str2, ft_strcapitalize(copia2));
-    printf("Original: '%s', Capitalizada:
-    '%s'\n", str3, ft_strcapitalize(copia3));
-    printf("Original: '%s', Capitalizada:
-    '%s'\n", str4, ft_strcapitalize(copia4));
-    printf("Original: '%s', Capitalizada:
-    '%s'\n", str5, ft_strcapitalize(copia5));
+    printf("Original: '%s', Capitalizada:'%s'\n", str1, ft_strcapitalize(copia1));
+    printf("Original: '%s', Capitalizada:'%s'\n", str2, ft_strcapitalize(copia2));
+    printf("Original: '%s', Capitalizada:'%s'\n", str3, ft_strcapitalize(copia3));
+    printf("Original: '%s', Capitalizada:'%s'\n", str4, ft_strcapitalize(copia4));
+    printf("Original: '%s', Capitalizada:'%s'\n", str5, ft_strcapitalize(copia5));
 
     free(copia1);
     free(copia2);
