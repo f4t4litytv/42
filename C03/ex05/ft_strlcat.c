a42215d2114d2bd6c85dfbca61a79b8315ee37d5
f4t4lity <f4t4lity@f4t4lity-G5-KD>
2025-03-18 17:35:06 +0000

    ex05 added

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size) {
+    unsigned int dest_len = 0;
+    unsigned int src_len = 0;
+    unsigned int total_len = 0;
+    unsigned int i = 0;
 
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
-{
-	unsigned int dest_len = 0;
-	unsigned int src_len = 0;
-	unsigned int total_len = 0;
-	unsigned int i = 0;
+    // Calcula o comprimento da string de destino
+    while (dest[dest_len] != '\0') {
+        dest_len++;
+    }
 
-	while (dest[dest_len] != '\0')
-	{
-		dest_len++;
-	}
+    // Calcula o comprimento da string de origem
+    while (src[src_len] != '\0') {
+        src_len++;
+    }
 
-	while (src[src_len] != '\0')
-	{
-		src_len++;
-	}
+    total_len = dest_len + src_len;
 
-	total_len = dest_len + src_len;
-
-	if (size > dest_len + 1)
-	{
-		while (src[i] != '\0' && dest_len +i < size - 1)
-		{
-			dest[dest_len + i] = src[i];
-			i++;
-		}
-		dest[dest_len + i] = '\0';
-		else if (size != dest_len)
-		{
-			dest[size-1] = '\0';
-		}
-	}
+    // Verifica se há espaço suficiente no buffer de destino
+    if (size > dest_len) {
+        if (size > dest_len + 1) {
+            while (src[i] != '\0' && dest_len + i < size - 1) {
+                dest[dest_len + i] = src[i];
+                i++;
+            }
+            dest[dest_len + i] = '\0'; // Garante que a string de destino seja terminada em nulo
+        } else if (size != dest_len) {
+            dest[size-1] = '\0';
+        }
+    }
+    return (total_len);
 }
 /*
 int main() {
