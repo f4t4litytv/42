024da4a9057ea7f0fdcf31e707b44a4a81ae9247
f4t4lity <f4t4lity@f4t4lity-G5-KD>
2025-03-20 18:27:49 +0000

    atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jpinto-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 17:00:14 by jpinto-r          #+#    #+#             */
-/*   Updated: 2025/03/20 17:31:16 by jpinto-r         ###   ########.fr       */
+/*   Updated: 2025/03/20 18:24:47 by jpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,51 @@ int	ft_atoi(char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		result = result * 10 (str[i] - '0');
+		result = result * 10 * (str[i] - '0');
 		i++;
 	}
 	return (result * sign);
+}
+
+// Protótipo da tua função ft_atoi
+int ft_atoi(char *str);
+
+int main() {
+    // Casos de teste
+    char *testes[] = {
+        "123",
+        "-456",
+        "  789",
+        "   -1011",
+        "0",
+        "-0",
+        "abc",
+        "123abc456",
+        "   +123",
+        "   -00123",
+        "   +00123",
+        "   -2147483648", // Valor mínimo de int
+        "   2147483647",  // Valor máximo de int
+        "   -2147483649", // Valor abaixo do mínimo de int
+        "   2147483648"   // Valor acima do máximo de int
+    };
+
+    // Número de casos de teste
+    int num_testes = sizeof(testes) / sizeof(testes[0]);
+
+    // Executar os testes
+    for (int i = 0; i < num_testes; i++) {
+        char *str = testes[i];
+        int resultado_ft_atoi = ft_atoi(str);
+        int resultado_atoi = ft_atoi(str); // Usar atoi como referência
+
+        // Comparar os resultados
+        if (resultado_ft_atoi == resultado_atoi) {
+            printf("Teste %d: SUCESSO - ft_atoi(\"%s\") = %d\n", i + 1, str, resultado_ft_atoi);
+        } else {
+            printf("Teste %d: FALHA - ft_atoi(\"%s\") = %d, esperado = %d\n", i + 1, str, resultado_ft_atoi, resultado_atoi);
+        }
+    }
+
+    return 0;
 }
